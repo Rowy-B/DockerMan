03cabb2c631554bc4fea5587cccbbeadd64f9ff6
rowyb <488796@student.fontys.nl>
2022-05-11 15:33:21 +0200

    een niet werkend output systeem

--- Main.go
+++ Main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 func main() {
+
+	_, Output := exec.Command("docker", "ps").Output()
+	//error := Cmd.Run()
+	fmt.Println(Output)
+	//if error != nil {
+	//	panic(error)
+	//}
 
 }
