2df56e885d0a5c466b34fb748b56e89742f3d9c9
rowyb <488796@student.fontys.nl>
2022-05-13 10:59:28 +0200

    webservice klaar met, docker ps, docker image & docker volume

--- Main.go
+++ Main.go
@@ -2,16 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os/exec"
 )
 
 func main() {
+	webbie()
+}
 
-	_, Output := exec.Command("docker", "ps").Output()
-	//error := Cmd.Run()
-	fmt.Println(Output)
-	//if error != nil {
-	//	panic(error)
-	//}
+func dockerPS() string {
+	out, err := exec.Command("docker", "ps", "-a").Output()
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(out)
+}
+
+func dockerImage() string {
+	out, err := exec.Command("docker", "image", "ls").Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out)
+}
+
+func dockerVolume() string {
+	out, err := exec.Command("docker", "volume", "ls").Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out)
+}
+
+func webbie() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "List of containers")
+		fmt.Fprintln(w, dockerPS())
+		fmt.Fprintln(w, "List of images")
+		fmt.Fprintln(w, dockerImage())
+		fmt.Fprintln(w, "List of volumes")
+		fmt.Fprintln(w, dockerVolume())
+	})
+
+	port := ":5000"
+	fmt.Println("Server is running on port" + port)
+
+	// Start server on port specified above
+	log.Fatal(http.ListenAndServe(port, nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module Main
 
 go 1.17
+
+require (
+	bitbucket.org/creachadair/shell v0.0.7 // indirect
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.15+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/sourcegraph/run v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
